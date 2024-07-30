fc20ff863ac16b2c38345eac38a8ea8f0065d23e
AntonioGMtz <antoniogallegos086@gmail.com>
2024-07-29 23:03:35 -0600

    First Steps / Login View

---
+++ app/src/main/java/com/example/notas/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.notas
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.notas.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Inicializar View Binding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
